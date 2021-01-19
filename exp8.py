15fe05348ee5464317c7ae4d07e80f4c58e14d6f
medashabari <72682329+medashabari@users.noreply.github.com>
2021-01-19 20:58:15 +0530

    Add files via upload

---
+++ exp8.py
@@ -0,0 +1,6 @@
+n = int(input("Enter n value : "))
+for r in range(n):
+    if(n+r+1 <= (2**r)):
+        print("The minimum value of r is : ",r)
+        break
+print("\n\n\n EXECUTED BY M.SHABARISH_19X51A0594")
